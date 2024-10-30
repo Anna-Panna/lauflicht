5cb1eb165400cb736ff3a8a190a366e37ce07699
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-30 13:56:24 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-let strip = neopixel.create(DigitalPin.P1, 32, NeoPixelMode.RGB)
-strip.setBrightness(50)
-basic.forever(function () {
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    status = 1
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
-    basic.pause(500)
-    strip.rotate(1)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    status = 0
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+})
+let status = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 32, NeoPixelMode.RGB)
+strip.setBrightness(50)
+status = 0
+strip.clear()
+basic.forever(function () {
+    if (status == 1) {
+        strip.show()
+        basic.pause(200)
+        strip.rotate(1)
+    }
+    for (let Index = 0; Index <= 32; Index++) {
+    	
+    }
 })
